c72fc21be12080c2a89c8b66998ad8dedb750f8e
choelzli <128326139+choelzli@users.noreply.github.com>
2023-09-16 04:30:27 -0400

    seeded database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,16 @@
--- insert data to pre-populate database
+INSERT INTO department (department_name)
+VALUES ("Accounting"),
+       ("Engineering"),
+       ("Legal"),
+       ("Marketing"),
+       ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant, 50000, 1"),
+       ("Staff Engineer, 55000, 2"),
+       ("Project Engineer, 85000, 2"),
+       ("Senior Engineer, 125000, 2"),
+       ("Legal Counsel, 110000, 3"),
+       ("Marketing Director, 130000, 4"),
+       ("Salesperson, 50000, 5"),
+       ("Lead Salesperson, 75000, 5")
